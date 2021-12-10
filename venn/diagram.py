08c832e3d8aa63b5abac8146e645441a28f19da7
OceanNuclear <annoyingamygdala@gmail.com>
2021-12-10 20:29:17 +0000

    changed the colouring scheme so it's clearer which part comes

--- venn/diagram.py
+++ venn/diagram.py
@@ -37,20 +37,29 @@ class PolygonVennDiagram():
             # A single triangle
             shape = _equidistribute_points_on_circle(3)
             offset_angle, scale_factor = 0, 1
+            # TODO: deal with this edge case
         elif self.N == 2:
             # return a rhombus, rotated 
             shape = ary([[0.5, 0], [0, 1], [-0.5, 0], [0, -1], [0.5, 0]])
             offset_angle, scale_factor = pi/2, 2
+            # TODO: deal with this edge case
         else:
             shape = _equidistribute_points_on_circle(self.N)
-            offset_angle, scale_factor = pi/self.N, 1/cos(pi/self.N) # isoceles triangle with unique angle = tau/N and leg=1 has height = cos(pi/self.N)
+            offset_angle, scale_factor = pi/self.N, cos(pi/self.N) # isoceles triangle with unique angle = tau/N and leg=1 has height = cos(pi/self.N)
 
-        coordinates = []
-        for layer in range(self.N):
-            coordinates.append(_rotate_and_scale(shape, offset_angle*layer, scale_factor**layer))
-        return coordinates
+            layered_coordinates = []
+            for layer in range(self.N):
+                layered_coordinates.append(_rotate_and_scale(shape, offset_angle*layer, scale_factor**layer))
+            layered_coordinates = ary(layered_coordinates)
+
+            sorted_to_components = []
+            for component in range(self.N):
+                indices = np.vstack([ary([[i, component] for i in range(self.N)]), ary([[self.N-1-i, (i+component+1)%self.N] for i in range(self.N)])])
+                sorted_to_components.append(layered_coordinates[indices.T[0], indices.T[1]])
+        return sorted_to_components
     # the increase in area between successive areas is 1/cos(pi/N)**2 -1.
     # I would prefer it if a different shape is chosen such that this is kept constant, i.e. independent of N.
+    # Or better yet, the area of each region should be the same.
 
     def plot(self, ax=None):
         import matplotlib.pyplot as plt
@@ -59,11 +68,12 @@ class PolygonVennDiagram():
         polygons = self.polygon_coordinates()
         xcoords = ary([p[:,0] for p in polygons]).T
         ycoords = ary([p[:,1] for p in polygons]).T
+        ax.set_aspect(1)
+        ax.set_xticks([]), ax.set_yticks([])
         return ax, ax.plot(xcoords, ycoords)
 
 if __name__=="__main__":
     # coords = PolygonVennDiagram(5).polygon_coordinates()
-    ax, lines = PolygonVennDiagram(5).plot()
+    ax, lines = PolygonVennDiagram(16).plot()
     import matplotlib.pyplot as plt
-    ax.set_aspect(1)
     plt.show()
